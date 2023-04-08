7f4434259d38cacc72ffd6d57e217f6c27c481af
lj140542 <jonathan.lefebvre76600@gmail.com>
2023-04-08 14:04:57 +0900

    Implements the display of the GameBoard

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-console.log('Hello World!');
+const buttons = document.getElementsByTagName('button');
+
+const Player = (name) => {
+  const getName = () => name;
+};
+
+const DisplayController = (() => {
+  const display = () => {
+    let board = GameBoard.getGameArray();
+    board.forEach(function (cell, index) {
+      buttons[index].innerHTML = cell;
+    });
+  };
+  const reset = () => {
+    Array.from(buttons).forEach(button => {
+      button.innerHTML = ""
+    });
+  };
+  return { display, reset };
+})();
+
+const GameBoard = (() => {
+  let GameArray = ['', '', '', '', 'X', '', '', '', ''];
+  const getGameArray = () => GameArray;
+  const reset = () => {
+    GameArray = [];
+  };
+  return { getGameArray, reset };
+})();
+
+DisplayController.display();
