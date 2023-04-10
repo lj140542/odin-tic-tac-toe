29deac77f2ab8c9a76528dde0cb927feefc243c1
lj140542 <jonathan.lefebvre76600@gmail.com>
2023-04-11 00:08:28 +0900

    Implements the checkVictory function
    - each turn all victory conditions are tested
    - if a player wins his name is displayed
    - every symbols are shrinked except the winning ones

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-const buttons = document.getElementsByClassName('board-button');
-const turnDisplay = document.getElementById('display-turn');
-
 const Player = (name, symbol) => {
   const getName = () => name;
   const getSymbol = () => symbol;
@@ -19,14 +16,24 @@ const DisplayController = (() => {
     });
   };
   const resetBoard = () => {
-    Array.from(buttons).forEach(button => {
+    buttons.forEach(button => {
       button.innerHTML = ""
     });
   };
   const displayTurn = () => {
     turnDisplay.innerHTML = `${currentPlayer.getName()} turn`;
   };
-  return { displayBoard, resetBoard, displayTurn };
+  const displayVictory = (winningArray) => {
+    turnDisplay.innerHTML = `${currentPlayer.getName()} wins !`;
+    console.log(winningArray);
+    buttons.forEach(button => {
+      console.log(`id:${button.id} | index:${winningArray.indexOf(button.id)}`);
+      if (winningArray.indexOf(button.id) == -1) {
+        button.classList.toggle('shrink');
+      }
+    });
+  };
+  return { displayBoard, displayTurn, displayVictory };
 })();
 
 const GameBoard = (() => {
@@ -37,24 +44,65 @@ const GameBoard = (() => {
   };
   const turnPlayed = (buttonId) => {
     let button = document.getElementById(buttonId);
-    let row = button.dataset.row-1;
-    let col = button.dataset.col-1;
+    let row = button.dataset.row - 1;
+    let col = button.dataset.col - 1;
+    let winningArray = [];
 
     if (gameArray[row][col] == '') {
       gameArray[row][col] = currentPlayer.getSymbol();
-      currentPlayer = currentPlayer === player1 ? player2 : player1;
       DisplayController.displayBoard();
-      DisplayController.displayTurn();
+      winningArray = checkVictory();
+      if (winningArray.length == 0) {
+        currentPlayer = currentPlayer === player1 ? player2 : player1;
+        DisplayController.displayTurn();
+      }
+      else { DisplayController.displayVictory(winningArray); }
     }
   };
-  return { getGameArray, reset, turnPlayed };
+  const checkVictory = () => {
+    // .. returns the ids of the buttons that represents the win
+    let symbol = currentPlayer.getSymbol();
+
+    // check for the rows 
+    if (gameArray[0][0] == symbol &&
+      gameArray[0][1] == symbol &&
+      gameArray[0][2] == symbol) { return ['1', '2', '3']; } // .. 1st row
+    if (gameArray[1][0] == symbol &&
+      gameArray[1][1] == symbol &&
+      gameArray[1][2] == symbol) { return ['4', '5', '6']; } // .. 2nd row
+    if (gameArray[2][0] == symbol &&
+      gameArray[2][1] == symbol &&
+      gameArray[2][2] == symbol) { return ['7', '8', '9']; } // .. 3rd row
+    // check for the cols
+    if (gameArray[0][0] == symbol &&
+      gameArray[1][0] == symbol &&
+      gameArray[2][0] == symbol) { return ['1', '4', '7']; } // .. 1st col
+    if (gameArray[0][1] == symbol &&
+      gameArray[1][1] == symbol &&
+      gameArray[2][1] == symbol) { return ['2', '5', '8']; } // .. 2nd col
+    if (gameArray[0][2] == symbol &&
+      gameArray[1][2] == symbol &&
+      gameArray[2][2] == symbol) { return ['3', '6', '9']; } // .. 3rd col
+    // check for the diagonals
+    if (gameArray[0][0] == symbol &&
+      gameArray[1][1] == symbol &&
+      gameArray[2][2] == symbol) { return ['1', '5', '9']; } // .. 1st diagonal
+    if (gameArray[0][2] == symbol &&
+      gameArray[1][1] == symbol &&
+      gameArray[2][0] == symbol) { return ['3', '5', '7']; } // .. 2nd diagonal
+
+    return [];
+  };
+  return { getGameArray, turnPlayed };
 })();
 
+const buttons = Array.from(document.getElementsByClassName('board-button'));
+const turnDisplay = document.getElementById('display-turn');
 const player1 = Player('Player 1', 'X');
 const player2 = Player('Player 2', 'O');
 let currentPlayer = player1;
 
-Array.from(buttons).forEach(button => {
+buttons.forEach(button => {
   button.addEventListener('click', e => GameBoard.turnPlayed(e.target.id))
 });
 
